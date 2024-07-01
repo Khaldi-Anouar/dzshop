59c3a683eb416a4898685bd54b3d6019b1056ac5
Khaldi-Anouar <anoare.khaldi@gmail.com>
2024-07-01 19:29:32 +0100

    initial commit

---
+++ assets/scales-BPd7HJoj.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BYWx67qE.js";const s=()=>e.jsx(e.Fragment,{children:"hello from Scales."});export{s as default};
---
+++ assets/teachers-C5tm8GJ_.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BYWx67qE.js";const s=()=>e.jsx(e.Fragment,{children:"hello from Teachers."});export{s as default};
